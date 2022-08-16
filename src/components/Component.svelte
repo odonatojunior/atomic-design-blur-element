d81f2bbb68414998f066d2972c92617aa9982b0d
Donato Junior <donato.junior.07@gmail.com>
2022-08-16 20:17:27 -0300

    feat: new method to emphasize component created

--- src/components/Component.svelte
+++ src/components/Component.svelte
@@ -1,24 +1,43 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
   function emphasizeClickedComponent({ target }) {
-    // const components = document.querySelectorAll(".isComponent");
-    // components.forEach((item) => {
-    //   console.log(item !== target.parentElement)
-    //   if(item === target){
-    //     target.classList.toggle('emphasis')
-    //   }
-    //   item.classList.toggle("blurComponent");
-    // });
+    const components = document.querySelectorAll(".isComponent");
+    const closestComponent = target.closest(".isComponent");
+
+    components.forEach((component) => {
+      if (component === closestComponent) {
+        component.classList.toggle("emphasis");
+      } else {
+        component.classList.toggle("blurComponent");
+      }
+    });
+  }
+
+  function removeEmphasisOnClickedComponent({ keyCode }) {
+    const components = document.querySelectorAll(".isComponent");
+    if (keyCode === 27) {
+      components.forEach((component) => {
+        component.classList.remove("emphasis", "blurComponent");
+      });
+    }
   }
 </script>
 
-<div class="isComponent" >
+<svelte:window on:keydown={removeEmphasisOnClickedComponent} />
+
+<div
+  class="isComponent"
+  on:contextmenu|preventDefault|stopPropagation={emphasizeClickedComponent}
+>
   <slot />
 </div>
 
 <style global>
   .isComponent.emphasis {
-    transform: scale(1.1);
-    filter: blur(0);
+    z-index: 500;
+    filter: blur(0px);
+    transform: scale(1.5);
   }
   .isComponent.blurComponent {
     filter: blur(2px);
